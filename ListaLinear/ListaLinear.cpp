d200b82d3fb7ad5607996a622fe29353abeaec49
Lucas Tavares <lucasamigo.eng@gmail.com>
2024-03-19 11:13:25 -0300

    At 03

--- ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear.cpp
@@ -122,7 +122,22 @@ void inserirElemento()
 
 void excluirElemento()
 {
+	int valor;
+	cout << "Digite o elemento que deseja excluir: ";
+	cin >> valor;
+	int pos = posicaoElemento(valor);
 
+	if (pos != -1) {
+		for (int i = pos; i < nElementos; i++) {
+			lista[i] = lista[i + 1];
+		}
+		cout << "O elemento " << valor << " foi excluido \n";
+		nElementos--;
+	}
+	else
+	{
+		cout << "O elemento digitado nao foi encontrado" << endl;
+	}
 
 }
 
